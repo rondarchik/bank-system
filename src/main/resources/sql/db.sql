aac5a6a9eb186c802391e08558ed9f8e4d302521
vdovgun@gmail.com <sergu9848@gmail.com>
2022-05-14 10:24:08 +0300

    db update (commit will be squashed later)

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -10,3 +10,65 @@ CREATE TABLE IF NOT EXISTS users
     role VARCHAR(100) NOT NULL
     )
     ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS banks
+(
+    id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+    )
+    ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS managers
+(
+    id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    bank_id INT NOT NULL,
+    user_id INT NOT NULL,
+
+    FOREIGN KEY (bank_id) REFERENCES banks (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+    )
+    ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS companies
+(
+    id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    bank_id INT NOT NULL,
+    type VARCHAR(100) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    unp VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+
+    FOREIGN KEY (bank_id) REFERENCES banks (id)
+    )
+    ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS clients
+(
+    id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    passport_id VARCHAR(255) NOT NULL,
+    identity_number VARCHAR(255) NOT NULL,
+    telephone VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    company_id INT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (company_id) REFERENCES companies (id),
+
+    )
+    ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS client_bank
+(
+    client_id INT NOT NULL,
+    bank_id INT NOT NULL,
+
+    FOREIGN KEY (client_id) REFERENCES clients (id),
+    FOREIGN KEY (bank_id) REFERENCES banks (id),
+
+    UNIQUE (client_id, bank_id)
+    )
+    ENGINE = InnoDB;
--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -4,4 +4,40 @@ VALUES (1, 'test1', '12345678', true , 'CLIENT'),
        (3, 'test3', '12345678', true, 'OPERATOR'),
        (4, 'test4', '12345678', true, 'MANAGER'),
        (5, 'test5', '12345678', true, 'THIRD_PARTY_SPECIALIST'),
-       (6, 'test6', '12345678', true , 'ADMIN');
+       (6, 'test6', '12345678', true , 'ADMIN'),
+       (7, 'test7', '12345678', true , 'MANAGER'),
+       (8, 'test8', '12345678', true , 'MANAGER');
+
+INSERT INTO banks
+VALUES (1, 'PRIOR BANK'),
+       (2, 'ALFA BANK'),
+       (3, 'BEL INVEST BANK');
+
+INSERT INTO managers
+VALUES (1, 1, 4),
+       (2, 2, 7),
+       (3, 3, 8);
+
+
+INSERT INTO companies
+VALUES (1, 1, 'OOO', 'ISSOFT', '1013125934', 'Partizansky avenue 1, Minsk, 220070, Belarus'),
+       (2, 1, 'OOO', 'EPAM', '1401125934', 'Partizansky avenue 2, Minsk, 220070, Belarus'),
+       (3, 1, 'OAO', 'ANDERSEN', '101125934', 'Partizansky avenue 3, Minsk, 220070, Belarus'),
+       (4, 2, 'OOO', 'PLAYTIKA', '134344343', 'Partizansky avenue 4, Minsk, 220070, Belarus'),
+       (5, 2, 'OAO', 'INNOWISE', '544322232', 'Partizansky avenue 5, Minsk, 220070, Belarus'),
+       (6, 2, 'OOO', 'LUX SOFT', '43434322', 'Partizansky avenue 6, Minsk, 220070, Belarus'),
+       (7, 3, 'OAO', 'IBA', '32323444', 'Partizansky avenue, Minsk 7, 220070, Belarus'),
+       (8, 3, 'OOO', 'Itransition', '32345777', 'Partizansky avenue 8, Minsk, 220070, Belarus'),
+       (9, 3, 'OOO', 'Exadel', '101125934', 'Partizansky avenue 9, Minsk, 220070, Belarus'),
+       (10, 3, 'OOO', 'SoftTeco', '101425998', 'Partizansky avenue 10, Minsk, 220070, Belarus');
+
+INSERT INTO clients
+VALUES (1, 1, 'Dovgun', 'Victoria', 'Alexandrovna', 'AB2718892992', 'B262332873323PB01', '+375292889911', 'rondarchik@proton.com', 1),
+       (2, 2, 'Dovgun', 'Elena', 'Alexandrovna', 'AB2718892993', 'B262332873323PB02', '+3752953368882', 'elennaaa@proton.com', 1);
+
+
+INSERT INTO client_bank
+VALUES (1, 1),
+       (1, 2),
+       (2, 1);
+
